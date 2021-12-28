e76e762fbae16be6acaf1e74440d74cf59c4dedc
SkuldNorniern <skuldnorniern@gmail.com>
2021-12-28 22:04:35 +0900

    [MODIFY] Updated some function
    / pom.xml
    /src
        /main
            /me.skuldnorniern.starteritem
                / listener.java

--- src/main/java/me/skuldnorniern/starteritem/listener.java
+++ src/main/java/me/skuldnorniern/starteritem/listener.java
@@ -1,6 +1,7 @@
 package me.skuldnorniern.starteritem;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -8,9 +9,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 public class listener implements Listener {
@@ -29,25 +32,30 @@ public class listener implements Listener {
             String name = p.getDisplayName();
             boolean bol = reslist.contains(name);
             System.out.println(reslist);
-            if(!bol) {
+            System.out.println(name);
+            System.out.println(reslist.contains(name));
+            if(bol) {
                 if (!(p.hasPlayedBefore())) {
                     ItemStack stack = new ItemStack(Material.STONE_AXE,1);
                     ItemMeta meta = stack.getItemMeta();
-                    meta.addEnchant(Enchantment.DURABILITY, 255, true);
+                    List<String> lore = Arrays.asList("Pre-Reservator's Item", " ", " ");
+                    meta.setDisplayName("A great communication method from the stone age");
+                    meta.setLore(lore);
+                    meta.setUnbreakable(true);
+                    meta.addEnchant(Enchantment.ARROW_INFINITE,1,true);
                     stack.setItemMeta(meta);
-
-                    Bukkit.broadcastMessage("Here is your reward! " + p.getDisplayName());
+                    //Damageable meda = ((Damageable) stack.getItemMeta());
+                    //meda.setDamage(1);
+                    //stack.setItemMeta(meda);
+                    Bukkit.broadcastMessage("Here is your pre reservation reward! " + p.getDisplayName());
                     p.getInventory().addItem(stack);
                 }
             }
             else
             {
                 ItemStack stack = new ItemStack(Material.STONE_AXE,1);
-                ItemMeta meta = stack.getItemMeta();
-                meta.addEnchant(Enchantment.DURABILITY, 255, true);
-                stack.setItemMeta(meta);
 
-                Bukkit.broadcastMessage("Here is your reward! " + p.getDisplayName());
+                Bukkit.broadcastMessage("Here is your welcome reward! " + p.getDisplayName());
                 p.getInventory().addItem(stack);
             }
         }
